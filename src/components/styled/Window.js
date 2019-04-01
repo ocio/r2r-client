a7d06a476b9d8c2dfc0c96a43656ed07d2810660
Josema <enzo@josema.es>
2019-04-01 12:36:40 +0200

    Making window layout

--- src/components/styled/Window.js
+++ src/components/styled/Window.js
@@ -4,16 +4,27 @@ import styled from '@emotion/styled'
 export default function Window({ width, height, children }) {
     return (
         <Container>
-            <Content width={width} height={height}>
-                {children}
-            </Content>
+            <Background width={width} height={height}>
+                <Background0 width={width} height={height} />
+                <Background1 />
+                <Background2 />
+                <Background3 />
+                <Background4 />
+                <Corners />
+                <Content>{children}</Content>
+            </Background>
         </Container>
     )
 }
 
+const WIDTH = 600
+const HEIGHT = 682
+
 const Container = styled.div`
     width: 100%;
     height: 100%;
+    top: 0;
+    left: 0;
     display: flex;
     align-items: center;
     justify-content: center;
@@ -21,11 +32,76 @@ const Container = styled.div`
     pointer-events: none;
 `
 
-const Content = styled.div`
+const Background = styled.div`
     width: ${p => p.width};
     height: ${p => p.height};
-    background-image: url('assets/window-left-top.png');
-    background-repeat: no-repeat;
-    /* background-size: 100% 100%; */
+    position: relative;
     pointer-events: all;
+`
+
+const Background1 = styled.div`
+    width: calc(100% - ${WIDTH}px);
+    height: ${HEIGHT / 2}px;
+    left: ${WIDTH / 2}px;
+    top: 0;
+    position: absolute;
+    background-image: url('assets/window-top.png');
+    background-repeat: repeat-x;
+`
+const Background2 = styled.div`
+    width: calc(100% - ${WIDTH}px);
+    height: ${HEIGHT / 2}px;
+    left: ${WIDTH / 2}px;
+    bottom: 0;
+    position: absolute;
+    background-image: url('assets/window-bottom.png');
+    background-repeat: repeat-x;
+`
+
+const Background3 = styled.div`
+    width: ${WIDTH / 2}px;
+    height: calc(100% - ${HEIGHT}px);
+    left: 0;
+    bottom: ${HEIGHT / 2}px;
+    position: absolute;
+    background-image: url('assets/window-left.png');
+    background-repeat: repeat-y;
+`
+
+const Background4 = styled.div`
+    width: ${WIDTH / 2}px;
+    height: calc(100% - ${HEIGHT}px);
+    right: 0;
+    bottom: ${HEIGHT / 2}px;
+    position: absolute;
+    background-image: url('assets/window-right.png');
+    background-repeat: repeat-y;
+`
+
+const Background0 = styled.div`
+    width: calc(${p => p.width} - 80px);
+    height: calc(${p => p.height} - 80px);
+    top: 40px;
+    left: 40px;
+    position: absolute;
+    background-color: #ffe7cd;
+`
+
+const Corners = styled.div`
+    width: 100%;
+    height: 100%;
+    position: relative;
+    background-image: url('assets/window-left-top.png'),
+        url('assets/window-right-top.png'), url('assets/window-left-bottom.png'),
+        url('assets/window-right-bottom.png');
+    background-position: left top, right top, left bottom, right bottom;
+    background-repeat: no-repeat, no-repeat, no-repeat, no-repeat;
+`
+
+const Content = styled.div`
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    bottom: 0;
 `
