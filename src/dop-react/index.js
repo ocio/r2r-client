7c12734253b109f17761839d84147651fdf73eb1
Josema <enzo@josema.es>
2019-05-07 11:09:57 +0200

    Optimized dop-react

--- src/dop-react/index.js
+++ src/dop-react/index.js
@@ -1,4 +1,10 @@
-import React, { createContext, useContext, useEffect, useState } from 'react'
+import React, {
+    createContext,
+    useContext,
+    useEffect,
+    useState,
+    useMemo
+} from 'react'
 import { createObserver, collectGetters } from 'dop'
 
 const Context = createContext()
@@ -13,18 +19,52 @@ export function useGlobalState() {
 
 export function useObserver() {
     const [value, setValue] = useState(true)
-    const observer = createObserver(() => setValue(!value)) // forceUpdate() trick
-    useEffect(() => () => observer.destroy())
+    const observer = useMemo(() => createObserver(() => {}), [])
+    observer.callback = () => setValue(!value) // forceUpdate() trick
+    useMount(() => () => observer.destroy())
     return observer
 }
 
 export function useAutoObserver() {
     const stopCollector = collectGetters()
     const observer = useObserver()
-    useEffect(() =>
+    useMount(() =>
         stopCollector().forEach(o =>
             observer.observeProperty(o.object, o.property)
         )
     )
     return observer
 }
+
+// https://twitter.com/_developit/status/1124857230149312513
+function useMount(fn) {
+    return useEffect(fn, [])
+}
+
+// const reducer = state => !state
+// const useForceUpdate = () => {
+//     const [, dispatch] = useReducer(reducer, true)
+//     const memoizedDispatch = useCallback(() => {
+//         dispatch(null)
+//     }, [dispatch])
+//     return memoizedDispatch
+// }
+
+// THE ELEGANT BUT UNOPTIMIZED VERSION
+// export function useObserver() {
+//     const [value, setValue] = useState(true)
+//     const observer = createObserver(() => setValue(!value)) // forceUpdate() trick
+//     useEffect(() => () => observer.destroy())
+//     return observer
+// }
+
+// export function useAutoObserver() {
+//     const stopCollector = collectGetters()
+//     const observer = useObserver()
+//     useEffect(() =>
+//         stopCollector().forEach(o =>
+//             observer.observeProperty(o.object, o.property)
+//         )
+//     )
+//     return observer
+// }
