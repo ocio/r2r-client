e43bc2a462f1d23150a24927056c0b9a5705faaf
Josema <enzo@josema.es>
2019-06-06 22:27:09 +0200

    Implemented basic header

--- src/components/views/Playing/Header.js
+++ src/components/views/Playing/Header.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import styled from '@emotion/styled'
+import { useGlobalState, useObserver } from 'dop-react'
 import Score from 'components/styled/Score'
+import { getPlayerIndex } from 'store/getters'
 
 export default function Header() {
+    const { game } = useGlobalState()
+    const observer = useObserver()
+    const player_index = getPlayerIndex({ game_id: game.id })
+    const info = game.players[player_index]
+    observer.observeAll(game.players)
     return (
         <HeaderSytled>
             <Score icon="assets/img/icon-score.png" highlight={true}>
                 0
             </Score>
-            <Score icon="assets/img/icon-kills.png">0</Score>
-            <Score icon="assets/img/icon-power.png">0</Score>
-            <Score icon="assets/img/icon-units.png">0</Score>
+            <Score icon="assets/img/icon-kills.png">{info.kills}</Score>
+            <Score icon="assets/img/icon-power.png">{info.power}</Score>
+            <Score icon="assets/img/icon-units.png">{info.units}</Score>
         </HeaderSytled>
     )
 }
