da77c79efe8b85fcdef9b59fd1bf04f22ec802e8
Josema <enzo@josema.es>
2019-08-09 11:32:22 +0200

    Added iogames link

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Provider } from 'dop-react'
+import styled from '@emotion/styled'
 import state from 'store/state'
 // Components
 import Background from 'components/styled/Background'
@@ -9,9 +10,38 @@ function App() {
     return (
         <Provider store={state}>
             <Views />
+            <Footer>
+                <a
+                    href="https://iogames.space"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    More .io games
+                </a>
+            </Footer>
             <Background />
         </Provider>
     )
 }
 
 export default App
+
+const Footer = styled.div`
+    position: absolute;
+    bottom: 0;
+    width: calc(100% - 20px);
+    /* background: blue; */
+    text-align: right;
+    /* margin-right: 20px; */
+
+    & a {
+        color: #1c453b;
+        font-size: 12px;
+        font-weight: bold;
+        letter-spacing: 0.2px;
+    }
+
+    & a:hover {
+        color: #317f69;
+    }
+`
